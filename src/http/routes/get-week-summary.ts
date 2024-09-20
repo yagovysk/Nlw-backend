9e9f11a3a8b327fedfa9e53f2b7029622d9c261f
yagovysk <yago09regis@gmail.com>
2024-09-20 00:01:08 -0300

    add

--- src/http/routes/get-week-summary.ts
+++ src/http/routes/get-week-summary.ts
@@ -2,14 +2,9 @@ import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { getWeekSummary } from "../../functions/get-week-summary";
 
 export const getWeekSummaryRoute: FastifyPluginAsyncZod = async (app) => {
-	app.get("/summary", async (request, reply) => {
-		console.log("GET /summary called"); // Log para verificar se a rota está sendo chamada
-		try {
-			const { summary } = await getWeekSummary();
-			return { summary };
-		} catch (error) {
-			console.error("Error fetching summary:", error); // Log de erro caso ocorra um problema
-			reply.status(500).send({ error: "Internal Server Error" });
-		}
+	app.get("/summary", async () => {
+		const { summary } = await getWeekSummary();
+
+		return { summary };
 	});
 };
