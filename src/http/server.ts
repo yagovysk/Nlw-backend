af1f188bc0032eb558707b8a89670097405369a2
yagovysk <yago09regis@gmail.com>
2024-09-19 23:10:57 -0300

    add front

--- src/http/server.ts
+++ src/http/server.ts
@@ -12,7 +12,12 @@ import { getWeekPendingGoalsRoute } from "./routes/get-week-pending-goals";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.register(fastifyCors, { origin: "*" });
+app.register(fastifyCors, {
+	origin:
+		process.env.NODE_ENV === "production"
+			? ["https://inorbityago.netlify.app/"] // Substitua pelo URL do seu frontend na Netlify
+			: "*",
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -22,6 +27,8 @@ app.register(createGoalCompletionRoute);
 app.register(getWeekSummaryRoute);
 app.register(getWeekPendingGoalsRoute);
 
-app.listen({ port: 3333 }).then(() => {
-	console.log("HTTP server running!");
+const port = Number(process.env.PORT) || 3333; // Converte para número ou usa 3333 como fallback
+
+app.listen({ port }).then(() => {
+	console.log(`HTTP server running on port ${port}`);
 });
